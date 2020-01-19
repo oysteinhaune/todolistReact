a89f51db32c1aff3c496c13f9bd88832a4aa9eff
Øystein Haugnes Aune <oysteinhaugnesaune@ysteins-MacBook-Pro.local>
2020-01-19 23:39:47 +0100

    add mysql connection

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,8 +1,44 @@
+require('dotenv').config()
 const express = require("express")
 const router = express.Router();
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'mysql'
+});
+
+const User = sequelize.define('user', {
+  // Model attributes are defined here
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  }
+}, {
+  // Other model options go here
+});
+
+const jane = User.build({ firstName: "Jane", lastName: "Johnson" });
+jane.save();
+console.log('Jane was saved to the database!');
+
+// `sequelize.define` also returns the model
+console.log(User === sequelize.models.User); // true
 
 router.get("/", (req, res) => {
-  res.send("Api is working properly")
+  try {
+  sequelize.authenticate();
+  res.send('Connection has been established successfully.');
+} catch (error) {
+  res.send('Unable to connect to the database:', error);
+}
 })
+
+
+
 
 module.exports = router
